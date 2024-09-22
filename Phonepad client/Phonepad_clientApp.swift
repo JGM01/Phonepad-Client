0afece795f3b99f57f9b64920db0f9a908155e8a
jgm0 <jacobgermanamccray@gmail.com>
2024-09-22 17:43:47 -0400

    Initial Commit

---
+++ Phonepad client/Phonepad_clientApp.swift
@@ -0,0 +1,17 @@
+//
+//  Phonepad_clientApp.swift
+//  Phonepad client
+//
+//  Created by Jacob Germana-Mccray on 9/22/24.
+//
+
+import SwiftUI
+
+@main
+struct Phonepad_clientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Phonepad clientTests/Phonepad_clientTests.swift
@@ -0,0 +1,17 @@
+//
+//  Phonepad_clientTests.swift
+//  Phonepad clientTests
+//
+//  Created by Jacob Germana-Mccray on 9/22/24.
+//
+
+import Testing
+@testable import Phonepad_client
+
+struct Phonepad_clientTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
